d24bdfad6a4ab7f9839f2e17d1d0ad325b553c5b
Sanket <32214164+sanket8088@users.noreply.github.com>
2023-01-10 23:28:55 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -2,8 +2,5 @@ print(2+2)
 print("code working fine")
 print("hdgahdasfdhafdhgasfh")
 print("my machine code")
-<<<<<<< HEAD
 print("jinal changes")
-=======
 print("david chnages")
->>>>>>> 3955f0b (added david chnages)
